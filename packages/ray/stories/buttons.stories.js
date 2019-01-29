5eb9621ba089393e2d33f92b4698106031281e91
adamraider <adamjraider@gmail.com>
2019-01-29 15:39:34 -0500

    feat(buttons): Add v1 button states (#4)

--- packages/ray/stories/buttons.stories.js
+++ packages/ray/stories/buttons.stories.js
@@ -5,8 +5,37 @@ import withPadding from './util/withPadding';
 
 storiesOf('Buttons', module)
   .addWithJSX('primary', () => (
-    <button className="btn--primary">Hello World</button>
+    <>
+      <button className="ray--btn ray--btn--primary">Click me</button>
+      <a href="javascript:;" className="ray--btn ray--btn--primary">
+        Anchor Tag
+      </a>
+      <button className="ray--btn ray--btn--primary ray--btn--compact">
+        Small clicks
+      </button>
+      <a
+        href="javascript:;"
+        className="ray--btn ray--btn--primary ray--btn--compact"
+      >
+        Compact Anchor
+      </a>
+
+      <button className="ray--btn ray--btn--primary ray--btn--compact" disabled>
+        Disabled
+      </button>
+    </>
   ))
   .addWithJSX('secondary', () => (
-    <button className="btn--secondary">Hello World</button>
+    <>
+      <button className="ray--btn ray--btn--secondary">Secondary</button>
+      <button className="ray--btn ray--btn--secondary ray--btn--compact">
+        Compact Secondary
+      </button>
+      <button
+        className="ray--btn ray--btn--secondary ray--btn--compact"
+        disabled
+      >
+        Disabled Secondary
+      </button>
+    </>
   ));
