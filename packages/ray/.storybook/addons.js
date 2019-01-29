43a4378ae784eddf6bcc7ff31648fb3895555b48
Adam Raider <adam.raider@wework.com>
2019-01-29 11:20:09 -0500

    docs(storybook): Better background for stories

--- packages/ray/.storybook/addons.js
+++ packages/ray/.storybook/addons.js
@@ -1,3 +1,5 @@
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-viewport/register';
 import 'storybook-addon-jsx/register';
+import '@storybook/addon-options/register';
+import '@storybook/addon-backgrounds/register';
--- packages/ray/.storybook/config.js
+++ packages/ray/.storybook/config.js
@@ -1,11 +1,22 @@
 import { configure, addDecorator, setAddon } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
 import { checkA11y } from '@storybook/addon-a11y';
-import { configureViewport } from '@storybook/addon-viewport';
 import JSXAddon from 'storybook-addon-jsx';
+import { withBackgrounds } from '@storybook/addon-backgrounds';
 import '../lib/application.scss';
+
+addDecorator(
+  withBackgrounds([{ name: 'checkers', value: 'url(/bg.svg)', default: true }])
+);
 
 setAddon(JSXAddon);
 addDecorator(checkA11y);
+
+addDecorator(
+  withOptions({
+    name: 'Ray'
+  })
+);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
