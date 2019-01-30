94166ebc994a0aa6b5fb40bf0aaaa0114aeccae7
Celeste Glavin <celeste.glavin@wework.com>
2019-01-30 17:36:21 -0500

    feat(buttons): Adds danger and tertiary buttons
    * Add tertiary to buttons
    
    * max button specs
    
    * Add compact danger
    
    * remove paddings for tertiary and update colors
    
    * small changes
    
    * add dividers and rename dangers
    
    * Add color for danger focus
    
    * remove danger color changes for hover and focus
    
    * disabled color

--- packages/ray/stories/buttons.stories.js
+++ packages/ray/stories/buttons.stories.js
@@ -3,39 +3,65 @@ import { storiesOf } from '@storybook/react';
 
 import withPadding from './util/withPadding';
 
+const Divider = () => <span style={{ width: '30px', display: 'inline-block' }}></span>;
+
 storiesOf('Buttons', module)
   .addWithJSX('primary', () => (
     <>
       <button className="ray--btn ray--btn--primary">Click me</button>
-      <a href="javascript:;" className="ray--btn ray--btn--primary">
-        Anchor Tag
-      </a>
+      <Divider />
+      <button className="ray--btn ray--btn--primary" disabled>Click me disabled</button>
+      <Divider />
       <button className="ray--btn ray--btn--primary ray--btn--compact">
-        Small clicks
+        Compact click me
       </button>
-      <a
-        href="javascript:;"
-        className="ray--btn ray--btn--primary ray--btn--compact"
-      >
-        Compact Anchor
-      </a>
-
+      <Divider />
       <button className="ray--btn ray--btn--primary ray--btn--compact" disabled>
-        Disabled
+        Compact Disabled
       </button>
+      <Divider />
+      <button className="ray--btn ray--btn--primary ray--btn--primaryDanger">Click me danger</button>
+      <Divider />
+      <button className="ray--btn ray--btn--primary ray--btn--compact ray--btn--primaryDanger">Compact click me danger</button>
     </>
   ))
   .addWithJSX('secondary', () => (
     <>
-      <button className="ray--btn ray--btn--secondary">Secondary</button>
+      <button className="ray--btn ray--btn--secondary">Click me</button>
+      <Divider />
+      <button className="ray--btn ray--btn--secondary" disabled>Click me disabled</button>
+      <Divider />
       <button className="ray--btn ray--btn--secondary ray--btn--compact">
-        Compact Secondary
+        Compact click me
       </button>
-      <button
-        className="ray--btn ray--btn--secondary ray--btn--compact"
-        disabled
-      >
-        Disabled Secondary
+      <Divider />
+      <button className="ray--btn ray--btn--secondary ray--btn--compact" disabled>
+        Compact Disabled
       </button>
+      <Divider />
+      <button className="ray--btn ray--btn--secondary ray--btn--secondaryDanger">Click me danger</button>
+      <Divider />
+      <button className="ray--btn ray--btn--secondary ray--btn--compact ray--btn--secondaryDanger">Compact click me danger</button>
+      <Divider />
+    </>
+  ))
+  .addWithJSX('tertiary', () => (
+    <>
+      <button className="ray--btn ray--btn--tertiary">Click me</button>
+      <Divider />
+      <button className="ray--btn ray--btn--tertiary ray--btn--compact">Compact click me</button>
+      <Divider />
+      <button className="ray--btn ray--btn--tertiary" disabled>
+        Disabled
+      </button>
+      <Divider />
+      <button className="ray--btn ray--btn--tertiary ray--btn--compact" disabled>
+        Compact disabled
+      </button>
+      <Divider />
+      <button className="ray--btn ray--btn--tertiary ray--btn--tertiaryDanger">Click me danger</button>
+      <Divider />
+      <button className="ray--btn ray--btn--tertiary ray--btn--compact ray--btn--tertiaryDanger">Compact click me danger</button>
+      <Divider />
     </>
   ));
