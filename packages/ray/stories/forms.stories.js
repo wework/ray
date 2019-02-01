294ef60ff87c04160d6b6bac9c2788ed64a19e73
Celeste Glavin <celeste.glavin@wework.com>
2019-02-01 15:55:24 -0500

    feat(forms): focused, placeholders, truncating, hints, & compact fields (#10)
    * fix text fields
    
    * support placeholders infields
    
    * use placeholder color light text
    
    * add truncating
    
    * Update lib/_forms.scss
    
    Co-Authored-By: cmugla <celeste.glavin@wework.com>
    
    * ellipsis stuff doesn't really work but its a little bit nicer
    
    * add IE prefix for placeholder-shown
    
    * remove prefix for IE, breaks everything
    
    * Add compact fields
    
    * match spec spacing:
    
    * add hint states
    
    * add border colors
    
    * fix variable refs and spacing issues with hints
    
    * reference 1rem in field-height calcs
    
    * oop misread the spec, correct label focused spacing
    
    * make --active class the true fallback
    
    * Update lib/_forms.scss
    
    Co-Authored-By: cmugla <celeste.glavin@wework.com>
    
    * add transition to placholder
    
    * Add disabled state
    
    * Update lib/_base.scss
    
    Co-Authored-By: cmugla <celeste.glavin@wework.com>

--- packages/ray/stories/forms.stories.js
+++ packages/ray/stories/forms.stories.js
@@ -4,18 +4,34 @@ import { storiesOf } from '@storybook/react';
 import withPadding from './util/withPadding';
 
 storiesOf('Forms', module)
-  .addWithJSX('FormControl', () =>
+  .addWithJSX('Text field', () =>
     withPadding(
-      <div className="FormControl">
-        <input id="email" type="text" placeholder="arya.stark@winterfell.com" />
+      <div className="ray--field">
+        <input id="email" type="email" placeholder="arya.stark@winterfell.org" />
         <label for="email">Email address</label>
       </div>
     )
   )
-  .addWithJSX('FormControl FormControl--active', () =>
+  .addWithJSX('Text field, active', () =>
     withPadding(
-      <div className="FormControl FormControl--active">
-        <input id="email" type="text" placeholder="arya.stark@winterfell.com" />
+      <div className="ray--field ray--field--active">
+        <input id="email" type="email" placeholder="arya.stark@winterfell.org" />
+        <label for="email">Email address</label>
+      </div>
+    )
+  )
+  .addWithJSX('Text field, disabled', () =>
+    withPadding(
+      <div className="ray--field">
+        <input id="email" type="email" placeholder="arya.stark@winterfell.org" disabled />
+        <label for="email">Email address</label>
+      </div>
+    )
+  )
+  .addWithJSX('Text field, active disabled', () =>
+    withPadding(
+      <div className="ray--field ray--field--active ray--field--disabled">
+        <input id="email" type="email" placeholder="arya.stark@winterfell.org" disabled />
         <label for="email">Email address</label>
       </div>
     )
@@ -26,28 +42,173 @@ storiesOf('Forms', module)
         <div className="row">
           <div className="offset-3 col-6">
             <h2 className="h3">Sign up</h2>
-            <div className="FormControl">
-              <input id="name" type="text" placeholder="Arya Stark" />
-              <label for="name">Name</label>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field">
+                <input id="name" type="text" placeholder="Arya Stark" />
+                <label for="name">Name</label>
+              </div>
             </div>
-            <div className="FormControl">
-              <input
-                id="email"
-                type="text"
-                placeholder="arya.stark@winterfell.com"
-              />
-              <label for="email">Email address</label>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field">
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="arya.stark@winterfell.org"
+                />
+                <label for="email">Email address</label>
+              </div>
+              <div className="ray--field__hint">We want your emails</div>
             </div>
-            <div className="FormControl">
-              <input
-                id="password"
-                type="password"
-                placeholder="the.north.remembers"
-              />
-              <label for="password">Password</label>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="*********"
+                />
+                <label for="password">Password</label>
+              </div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field">
+                <input id="long-label" type="text" placeholder="Super soopah soup or super long placeholder" />
+                <label for="long-label">Label Empty State That Overflows quickly and is very long</label>
+              </div>
             </div>
 
-            <button className="btn--primary">Submit</button>
+            <button className="ray--btn ray--btn--primary">Submit</button>
+          </div>
+        </div>
+      </div>
+    )
+  )
+  .addWithJSX('Hint states', () =>
+    withPadding(
+      <div className="container">
+        <div className="row">
+          <div className="offset-3 col-6">
+            <div className="ray--fieldWrapper">
+              <div className="ray--field">
+                <input id="name" type="text" placeholder="Arya Stark" />
+                <label for="name">Name</label>
+              </div>
+              <div className="ray--field__hint">Nuetral hint</div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--success">
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="arya.stark@winterfell.org"
+                />
+                <label for="email">Email address</label>
+              </div>
+              <div className="ray--field__hint ray--field__hint--success">Success hint</div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--error">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="*********"
+                />
+                <label for="password">Password</label>
+              </div>
+              <div className="ray--field__hint ray--field__hint--error">Error hint</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  )
+  .addWithJSX('Text field - compact', () =>
+    withPadding(
+      <div className="ray--field ray--field--compact">
+        <input id="email" type="email" placeholder="arya.stark@winterfell.org" />
+        <label for="email">Email address</label>
+      </div>
+    )
+  )
+  .addWithJSX('Example form - compact', () =>
+    withPadding(
+      <div className="container">
+        <div className="row">
+          <div className="offset-3 col-6">
+            <h2 className="h3">Sign up</h2>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--compact">
+                <input id="name" type="text" placeholder="Arya Stark" />
+                <label for="name">Name</label>
+              </div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--compact">
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="arya.stark@winterfell.org"
+                />
+                <label for="email">Email address</label>
+              </div>
+              <div className="ray--field__hint">We want your emails</div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--compact">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="*********"
+                />
+                <label for="password">Password</label>
+              </div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--compact">
+                <input id="long-label" type="text" placeholder="Super soopah soup or super long placeholder" />
+                <label for="long-label">Label Empty State That Overflows quickly and is very long</label>
+              </div>
+            </div>
+
+            <button className="ray--btn ray--btn--compact ray--btn--primary">Submit</button>
+          </div>
+        </div>
+      </div>
+    )
+  )
+  .addWithJSX('Hint states - compact', () =>
+    withPadding(
+      <div className="container">
+        <div className="row">
+          <div className="offset-3 col-6">
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--compact">
+                <input id="name" type="text" placeholder="Arya Stark" />
+                <label for="name">Name</label>
+              </div>
+              <div className="ray--field__hint">Nuetral hint</div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--success ray--field--compact">
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="arya.stark@winterfell.org"
+                />
+                <label for="email">Email address</label>
+              </div>
+              <div className="ray--field__hint ray--field__hint--success">Success hint</div>
+            </div>
+            <div className="ray--fieldWrapper">
+              <div className="ray--field ray--field--error ray--field--compact">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="*********"
+                />
+                <label for="password">Password</label>
+              </div>
+              <div className="ray--field__hint ray--field__hint--error">Error hint</div>
+            </div>
           </div>
         </div>
       </div>
