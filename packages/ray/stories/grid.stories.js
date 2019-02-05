52441eaf17c952a00b1dc1d100f37a2121572873
adamraider <adamjraider@gmail.com>
2019-02-05 10:29:56 -0500

    feat(grid): Replace bootstrap grid with material (#16)
    * feat(grid): Replace bootstrap grid with material
    
    * chore: Remove unused grid story
    
    * Add prefix, remove extra files

--- packages/ray/stories/grid.stories.js
+++ packages/ray/stories/grid.stories.js
@@ -3,29 +3,26 @@ import { storiesOf } from '@storybook/react';
 import { throttle, range } from 'lodash';
 import withPadding from './util/withPadding';
 
-storiesOf('Grid', module)
-  .addWithJSX('default', () => (
-    <div style={{ backgroundColor: 'hsl(144, 100%, 75%)' }}>
-      <div className="ray--container">
-        <div
-          className="ray--row"
-          style={{ backgroundColor: 'hsl(0, 100%, 75%)' }}
-        >
-          {range(12).map(n => (
-            <div className="ray--col-1">
-              <div
-                style={{
-                  minHeight: '100vh',
-                  paddingBottom: '100%',
-                  backgroundColor: 'hsl(255, 100%, 75%)'
-                }}
-              >
-                {n + 1}
-              </div>
+storiesOf('Grid', module).addWithJSX('default', () => (
+  <div style={{ backgroundColor: 'hsl(144, 100%, 75%)' }}>
+    <div className="ray--grid">
+      <div
+        className="ray--grid__inner"
+        style={{ backgroundColor: 'hsl(0, 100%, 75%)' }}
+      >
+        {range(12).map(n => (
+          <div className="ray--grid__cell--span-1">
+            <div
+              style={{
+                minHeight: '4vh',
+                backgroundColor: 'hsl(255, 100%, 75%)'
+              }}
+            >
+              {n + 1}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
-  ))
-  .add('ray--container', () => <div className="ray--container">asdsads</div>);
+  </div>
+));
