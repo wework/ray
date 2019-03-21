7bfde2eb3ce9d903ab8786fc0830ce658175419a
adamraider <adamjraider@gmail.com>
2019-03-21 11:56:50 -0400

    ci: add stylelint (#69)
    * ci: add stylelint base config
    
    * refactor: fix stylelint errors
    
    * remoev unused package

---
+++ stylelint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rules: {
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['export']
+      }
+    ],
+    'selector-pseudo-element-no-unknown': true,
+    'selector-type-no-unknown': true,
+    'selector-class-pattern':
+      '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
+    'no-eol-whitespace': true,
+    'max-empty-lines': 2,
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment']
+      }
+    ]
+  }
+};
