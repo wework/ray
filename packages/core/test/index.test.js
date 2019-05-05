4019f03f6ff04d5342d199975cc45fe918aabdc9
adamraider <adam.raider@wework.com>
2019-05-05 13:44:17 -0400

    Update index.test.js

--- packages/core/test/index.test.js
+++ packages/core/test/index.test.js
@@ -1,6 +1,6 @@
 describe('RayCore', () => {
   describe('library can be imported', () => {
-    test('it can be imported with no errors', done => {
+    test('with no errors thrown', done => {
       expect(async () => {
         await require('../src'); // eslint-disable-line global-require
         done();
