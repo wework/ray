8208ff9e1423200bbd11333eb66d6262a320461e
Alan Friedman <d.alan.friedman@gmail.com>
2019-05-08 16:48:20 -0400

    feat(settings): export a better way

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,8 +1,7 @@
 import './global/js/polyfills';
 import boot from './global/js/boot';
-import settingsObj from './global/js/settings';
 
 boot();
 
 export * from './components';
-export const settings = settingsObj;
+export { default as settings } from './global/js/settings';
