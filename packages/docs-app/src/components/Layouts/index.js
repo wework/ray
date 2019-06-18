d8d18a8533f8c4d36d3ac42c00a6f449dfec9932
Adam Raider <adamjraider@gmail.com>
2019-06-18 13:12:11 -0400

    fix html base

--- packages/docs-app/src/components/Layouts/index.js
+++ packages/docs-app/src/components/Layouts/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
 import SideNav from '../SideNav';
 import Search from '../Search';
-import favicon32 from '../../content/global/images/favicon-32.png';
-import '../../styles/index.scss';
+import HtmlBase from './html';
 
 class Layout extends React.Component {
   static propTypes = {
@@ -30,67 +27,32 @@ class Layout extends React.Component {
     const { children } = this.props;
 
     return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
-          }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                {
-                  name: 'description',
-                  content: `Resources for building interfaces with WeWork's design system.`
-                },
-                {
-                  name: 'keywords',
-                  content: 'design system, wework, ray'
-                }
-              ]}
-              link={[
-                {
-                  rel: 'shortcut icon',
-                  type: 'image/png',
-                  href: `${favicon32}`
-                }
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <div className="page">
-              <div className="ray-page-container">
-                <div className="ray-grid">
-                  <div className="ray-grid__cell--span-3">
-                    <SideNav
-                      location={this.props.location}
-                      clickToClose={this.clickToClose}
-                      open={this.state.navOpen}
-                      toggleNav={this.toggleNav}
-                    />
-                  </div>
+      <HtmlBase>
+        <div className="page">
+          <div className="ray-page-container">
+            <div className="ray-grid">
+              <div className="ray-grid__cell--span-3">
+                <SideNav
+                  location={this.props.location}
+                  clickToClose={this.clickToClose}
+                  open={this.state.navOpen}
+                  toggleNav={this.toggleNav}
+                />
+              </div>
 
-                  <div className="ray-grid__cell ray-grid__cell--span-8-tablet ray-grid__cell--span-9-desktop">
-                    <div
-                      className="ray-page-container"
-                      style={{ marginTop: '1.5rem' }}
-                    >
-                      <Search />
-                    </div>
-                    <main className="main-content">{children}</main>
-                  </div>
+              <div className="ray-grid__cell ray-grid__cell--span-8-tablet ray-grid__cell--span-9-desktop">
+                <div
+                  className="ray-page-container"
+                  style={{ marginTop: '1.5rem' }}
+                >
+                  <Search />
                 </div>
+                <main className="main-content">{children}</main>
               </div>
             </div>
-          </>
-        )}
-      />
+          </div>
+        </div>
+      </HtmlBase>
     );
   }
 }
--- packages/docs-app/src/pages/index.js
+++ packages/docs-app/src/pages/index.js
@@ -1,75 +1,78 @@
 import React from 'react';
 
 import '../polyfills';
+import HtmlBase from '../components/Layouts/html';
 
 class Homepage extends React.Component {
   render() {
     return (
-      <div className="ray-page-container">
-        <div
-          className="ray-grid"
-          style={{
-            minHeight: '100vh',
-            alignItems: 'center'
-          }}
-        >
-          <div className="ray-grid__cell--span-full ray-grid__cell--push-1-desktop">
-            <div className="ray-text--h1" style={{ marginBottom: '0.5rem' }}>
-              Ray
-              <br />
-              Design
-              <span
-                className="ray-text--body-small ray-text--monospace"
+      <HtmlBase>
+        <div className="ray-page-container">
+          <div
+            className="ray-grid"
+            style={{
+              minHeight: '100vh',
+              alignItems: 'center'
+            }}
+          >
+            <div className="ray-grid__cell--span-full ray-grid__cell--push-1-desktop">
+              <div className="ray-text--h1" style={{ marginBottom: '0.5rem' }}>
+                Ray
+                <br />
+                Design
+                <span
+                  className="ray-text--body-small ray-text--monospace"
+                  style={{
+                    marginLeft: '1rem',
+                    marginBottom: '1rem',
+                    opacity: 0.7
+                  }}
+                >
+                  by WeWork
+                </span>
+              </div>
+
+              <div
+                className="ray-text--body-large"
                 style={{
-                  marginLeft: '1rem',
-                  marginBottom: '1rem',
+                  marginBottom: '2rem',
                   opacity: 0.7
                 }}
               >
-                by WeWork
-              </span>
-            </div>
+                Resources for building user interfaces
+                <br />
+                with the WeWork design language.
+              </div>
 
-            <div
-              className="ray-text--body-large"
-              style={{
-                marginBottom: '2rem',
-                opacity: 0.7
-              }}
-            >
-              Resources for building user interfaces
-              <br />
-              with the WeWork design language.
-            </div>
-
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center'
-              }}
-            >
-              <a
-                href="/getting-started"
-                className="ray-button ray-button--primary"
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center'
+                }}
               >
-                Docs
-              </a>
-              <a
-                href="https://github.com/wework/ray"
-                className="ray-button ray-button--secondary"
-              >
-                GitHub
-              </a>
-              <a
-                href="https://www.npmjs.com/package/@wework/ray-core"
-                className="ray-button ray-button--tertiary ray-text--monospace"
-              >
-                yarn add @wework/ray-core
-              </a>
+                <a
+                  href="/getting-started"
+                  className="ray-button ray-button--primary"
+                >
+                  Docs
+                </a>
+                <a
+                  href="https://github.com/wework/ray"
+                  className="ray-button ray-button--secondary"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://www.npmjs.com/package/@wework/ray-core"
+                  className="ray-button ray-button--tertiary ray-text--monospace"
+                >
+                  yarn add @wework/ray-core
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </HtmlBase>
     );
   }
 }
