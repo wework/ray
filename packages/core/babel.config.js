c947915a9c48ab383c2c1ad2190e891f474bd065
Celeste Glavin <celeste.glavin@wework.com>
2019-07-25 14:28:19 -0400

    fix: add browser support to ray-core (#182)

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
-          node: 'current'
+          node: 'current',
+          browsers: ['> 5%', 'Last 3 versions and not ie < 11']
         },
         ...(process.env.NODE_ENV === 'test'
           ? { modules: 'auto' }
