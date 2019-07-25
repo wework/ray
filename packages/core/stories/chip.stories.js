38fd572be8dee794b5b083f2b910801e6fd03531
Gabrielle Harrison <gabrielle.harrison@wework.com>
2019-07-25 14:47:57 -0400

    adds rtl story

--- packages/core/stories/chip.stories.js
+++ packages/core/stories/chip.stories.js
@@ -21,6 +21,19 @@ storiesOf('Chip', module).add('default', () => {
     </>
   );
 });
+storiesOf('Chip', module).add('default RTL', () => {
+  setTimeout(initChip);
+  return (
+    <div dir="rtl">
+      <div tabIndex="1" className="ray-chip">
+        Neighborhoods
+      </div>
+      <div tabIndex="1" className="ray-chip ray-chip--active">
+        Offices
+      </div>
+    </div>
+  );
+});
 storiesOf('Chip', module).add('micro', () => {
   setTimeout(initChip);
   return (
@@ -69,15 +82,37 @@ storiesOf('Chip', module).add('with icon', () => {
     </div>
   );
 });
-storiesOf('Chip', module).add('micro', () => {
+storiesOf('Chip', module).add('RTL with icon', () => {
   setTimeout(initChip);
   return (
-    <div>
-      <div tabIndex="1" className="ray-chip ray-chip--micro">
-        Offices
+    <div dir="rtl">
+      <div tabIndex="1" className="ray-chip">
+        <div className="ray-chip__icon-wrapper">
+          <svg width="10" height="10" viewBox="0 0 10 10">
+            <rect
+              width="10"
+              height="10"
+              strokeWidth="2"
+              stroke="#0000FF"
+              fill="#FFFFFF"
+            />
+          </svg>
+          <span className="ray-chip__text">Furnished</span>
+        </div>
       </div>
-      <div tabIndex="1" className="ray-chip ray-chip--micro ray-chip--active">
-        Offices
+      <div tabIndex="1" className="ray-chip ray-chip--active">
+        <div className="ray-chip__icon-wrapper">
+          <svg width="10" height="10" viewBox="0 0 10 10">
+            <rect
+              width="10"
+              height="10"
+              strokeWidth="2"
+              stroke="#0000FF"
+              fill="#FFFFFF"
+            />
+          </svg>
+          <span className="ray-chip__text">Offices</span>
+        </div>
       </div>
     </div>
   );
