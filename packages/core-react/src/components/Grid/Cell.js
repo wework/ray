0883b15a0a542fd34a29ec71c2bf411b8d6d72b1
Adam Raider <adam.raider@wework.com>
2019-07-31 16:24:06 -0700

    fix tests

--- packages/core-react/src/components/Grid/Cell.js
+++ packages/core-react/src/components/Grid/Cell.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { settings } from '@wework/ray-core';
 
-const breakpoints = Object.keys(settings.breakpointsInPixels);
+const BREAKPOINTS = ['phone', 'tablet', 'desktop'];
 
 export default function Cell({ className, push, span, ...props }) {
-  const breakpointClasses = breakpoints.reduce((memo, breakpointName) => {
+  const breakpointClasses = BREAKPOINTS.reduce((memo, breakpointName) => {
     const spanKey = `span-${breakpointName}`;
     if (props[spanKey]) {
       memo.push(`ray-grid__cell--span-${props[spanKey]}-${breakpointName}`);
