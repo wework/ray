2b1f5f14d9e07b2d584466ae06f3046ba6db8bb3
Ramin B <ramin@wework.com>
2019-08-05 18:03:15 -0400

    feat(core-react): work on react card and aspect ratios

--- packages/core-react/src/components/Card/index.js
+++ packages/core-react/src/components/Card/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
-export default function Card({ heading, body, img, row, className }) {
+import rayExports from '@wework/ray-core/scss/ray-exports.scss';
+
+const aspectRatios = (rayExports['ray-image-aspect-ratios'] || '').split(' ');
+
+export default function Card({
+  heading,
+  body,
+  img,
+  row,
+  aspectRatio,
+  className,
+  ...props
+}) {
   return (
-    <div className={clsx('ray-card', { 'ray-card--row': row }, className)}>
-      <div className="ray-card__image ray-image ray-image--16by9">{img}</div>
+    <div
+      {...props}
+      className={clsx('ray-card', { 'ray-card--row': row }, className)}
+    >
+      <div
+        className={clsx('ray-card__image', 'ray-image', {
+          [`ray-image--${aspectRatio}`]: aspectRatio
+        })}
+      >
+        {img}
+      </div>
       <div className="ray-card__content">
         <div className="ray-card__heading">{heading}</div>
         <div>{body}</div>
@@ -19,5 +40,11 @@ Card.propTypes = {
   body: PropTypes.node,
   img: PropTypes.node,
   row: PropTypes.bool,
+  aspectRatio: PropTypes.oneOf(aspectRatios),
   className: PropTypes.string
+};
+
+Card.defaultProps = {
+  row: false,
+  aspectRatio: '16by9'
 };
