32269fd689d7a7d493739ee488f5c37e5d6a76ec
Ramin B <ramin@wework.com>
2019-08-06 15:04:02 -0400

    add mocks for jest

--- packages/core-react/jest.config.js
+++ packages/core-react/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   displayName: '@wework/ray-core-react',
   testMatch: ['<rootDir>/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/setup.js']
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js'
+  }
 };
