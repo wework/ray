7144ec2967f6db070a4de0eed9ade1bb83b13fed
Adam Raider <adamjraider@gmail.com>
2019-08-15 15:01:42 -0400

    feat(core-react): add image (#198)
    * feat(core-react): add image WIP
    
    * add basic unit tests

---
+++ packages/core-react/src/components/Image/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+
+function ratioToPercentageString(ratio) {
+  return `${100 / ratio}%`;
+}
+
+const ASPECT_RATIOS_MAP = {
+  '16by9': ratioToPercentageString(16 / 9),
+  '1by1': ratioToPercentageString(1),
+  '3by4': ratioToPercentageString(3 / 4),
+  '4by3': ratioToPercentageString(4 / 3),
+  cinema: ratioToPercentageString(2.4)
+};
+
+const ASPECT_RATIOS = Object.keys(ASPECT_RATIOS_MAP);
+
+export default function Image({ className, src, aspect, bg, ...rest }) {
+  if (bg) {
+    return (
+      <div
+        className={clsx(
+          'ray-bg',
+          {
+            [`ray-bg--${aspect}`]: aspect
+          },
+          className
+        )}
+        {...rest}
+        style={{
+          ...rest.style,
+          backgroundImage: `url(${src})`
+        }}
+      />
+    );
+  }
+
+  return (
+    <div
+      className={clsx('ray-image', {
+        [`ray-image--${aspect}`]: aspect
+      })}
+    >
+      <img className={className} src={src} {...rest} />
+    </div>
+  );
+}
+
+Image.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.string,
+  aspect: PropTypes.oneOf(Object.keys(ASPECT_RATIOS)),
+  bg: PropTypes.bool
+};
