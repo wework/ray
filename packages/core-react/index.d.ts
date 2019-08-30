b123453fcf891432a3817a23319dfcfeeb8a91b2
Joey Gomez-Benito <joey.gomez-benito@wework.com>
2019-08-30 15:01:26 -0700

    chore(core-react): add typings for button

--- packages/core-react/index.d.ts
+++ packages/core-react/index.d.ts
@@ -8,4 +8,16 @@ export type TextFieldProps = {
 
 declare const TextField: React.FC<TextFieldProps>;
 
-export { TextField };
+export type ButtonProps = {
+  Tag?: 'button' | 'a';
+  children?: React.ReactNode;
+  type?: 'primary' | 'secondary' | 'tertiary';
+  compact?: boolean;
+  danger?: boolean;
+  disabled?: boolean;
+  className?: string;
+};
+
+declare const Button: React.FC<ButtonProps>;
+
+export { TextField, Button };
