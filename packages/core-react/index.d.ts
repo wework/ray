a2ac0594ccaa55bd667a55028110822d47ce63fe
Smith Suth <seimith.suth@wework.com>
2019-09-03 15:40:08 -0700

    Add types for card

--- packages/core-react/index.d.ts
+++ packages/core-react/index.d.ts
@@ -9,3 +9,15 @@ export type TextFieldProps = {
 declare const TextField: React.FC<TextFieldProps>;
 
 export { TextField };
+
+export type CardProps = {
+  body?: React.ReactNode,
+  className?: string,
+  heading?: React.ReactNode,
+  img?: React.ReactNode,
+  row?: boolean,
+};
+
+declare const Card: React.FC<CardProps>;
+
+export { TextField, Card };
