0edb8cf859e30bef8b9e5f72b032ec552de7b17a
Smith Suth <seimith.suth@wework.com>
2019-09-03 16:18:15 -0700

    Add types for radio

--- packages/core-react/index.d.ts
+++ packages/core-react/index.d.ts
@@ -8,4 +8,13 @@ export type TextFieldProps = {
 
 declare const TextField: React.FC<TextFieldProps>;
 
-export { TextField };
+export type RadioProps = {
+  className?: string,
+  id: string,
+  label?: React.ReactNode
+  name?: string,
+} & React.HTMLProps<HTMLInputElement>;
+
+declare const Radio: React.FC<RadioProps>;
+
+export { TextField, Radio };
