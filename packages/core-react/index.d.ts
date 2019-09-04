792916c9ec9a7eab513385bc94623976124f659e
Morgan Plant <Morgantheplant@gmail.com>
2019-09-04 16:26:06 -0700

    feat(core-react): add typings to index.d.ts for Container, Grid, & Cell (#218)

--- packages/core-react/index.d.ts
+++ packages/core-react/index.d.ts
@@ -8,6 +8,23 @@ export type TextFieldProps = {
 
 declare const TextField: React.FC<TextFieldProps>;
 
+export type ContainerProps = {
+  Tag?: React.ElementType<any>;
+} & React.HTMLProps<HTMLElement>;
+
+declare const Container: React.FC<ContainerProps>;
+
+export type GridProps = React.HTMLProps<HTMLElement>;
+
+declare const Grid: React.FC<GridProps>;
+
+export type CellProps = {
+  push?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  span?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+} & React.HTMLProps<HTMLElement>;
+
+declare const Cell: React.FC<CellProps>;
+
 export type ButtonProps = {
   Tag?: 'button' | 'a';
   children?: React.ReactNode;
@@ -20,4 +37,9 @@ export type ButtonProps = {
 
 declare const Button: React.FC<ButtonProps>;
 
-export { TextField, Button };
+export { 
+  Button, 
+  Container, 
+  Grid, 
+  TextField, 
+};
