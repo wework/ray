da7df55acc2b5cad59452dbb8c30ed170fdb9536
James Etienne <james.etienne@wework.com>
2019-09-23 15:38:37 -0400

    added changes to toast

---
+++ packages/product-react/src/components/Toast/Toast.tsx
@@ -0,0 +1,102 @@
+import clsx from 'clsx';
+import React from 'react';
+import '../Toast/Toast.scss';
+
+// import '../Select/Select.scss';
+
+export interface TagProps extends React.HTMLProps<HTMLButtonElement> {
+  label?: string;
+  open?: boolean;
+  variant?: string;
+  animation?: boolean;
+  index?: number;
+  onClick?(): void;
+}
+
+// const cx = clsx.bind(styles);
+
+export const Toast: React.FC<TagProps> = ({
+  label,
+  open,
+  variant,
+  animation,
+  index,
+  onClick,
+  ...rest
+}) => {
+  const offsetStyle = {
+    transform: `translateY(calc(50vh - 30px - (${index}*${1.9}*65px)))`
+  };
+  const [dropin, setAnimationDropIn] = React.useState(true);
+  const [offset, setOffsetstyle] = React.useState(offsetStyle);
+
+  React.useEffect(() => {
+    console.log(
+      'useEffect>>>>>',
+      index,
+      '****',
+      label !== undefined ? label.length : ''
+    );
+    setAnimationDropIn(false);
+    const timeout = setTimeout(
+      () => {
+        const offsetStyle = {
+          transform: '',
+          opacity: 0
+        };
+        setOffsetstyle(offsetStyle);
+      },
+      index !== undefined ? index * 1000 : 1000
+    );
+    return () => clearTimeout(timeout);
+  }, []);
+
+  let withaction: string = '';
+  let withoutaction: string = '';
+  const success: boolean = variant === 'success';
+  const error: boolean = variant === 'error';
+  const warning: boolean = variant === 'warning';
+  const original: boolean = variant === 'original';
+
+  if (onClick !== undefined) {
+    withaction = 'toast-with-action';
+  } else {
+    withoutaction = 'toast-without-action';
+  }
+
+  const classes = clsx({
+    'ray-toast-with-action': withaction,
+    'ray-toast-without-action': withoutaction,
+    'ray-toast--error': error,
+    'ray-toast--success': success,
+    'ray-toast--warning': warning,
+    'ray-toast--original': original,
+    'ray-toast-drop-in': dropin
+  });
+
+  return (
+    <div>
+      <span className={classes} style={offset}>
+        <span className="ray-toast">{label}</span>
+        <span className="ray-toast ray-toast-float">
+          {withaction !== '' && (
+            <a className="ray-toast-action" onClick={onClick}>
+              Undo
+            </a>
+          )}
+        </span>
+        {/* <span className="toast-close">x</span> */}
+      </span>
+    </div>
+  );
+};
+
+Toast.defaultProps = {
+  label: '',
+  open: false,
+  variant: 'default',
+  animation: true,
+  index: 0
+};
+
+export default Toast;
---
+++ packages/product-react/stories/Toast.story.tsx
@@ -0,0 +1,50 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { Toast } from '../src/components/Toast/Toast';
+
+const undoAction = () => {
+  console.log('undooo stuf');
+};
+
+storiesOf('Core Comps | Toast', module)
+  .add('Multi toast success', () => {
+    return [1, 2, 3,4,5,6,7,8,9].map((x, index) => (
+      <Toast
+        key={index}
+        label="hey  am Grant Component Toast Grant Component Toast Grant Component Toast"
+        variant="success"
+        index={index}
+      />
+    ));
+  })
+  .add('Multi toast success and action', () => {
+    return [1, 2, 3].map((x, index) => (
+      <Toast
+        key={index}
+        label="hey there,I am Grant Component Toast I am Grant Component ToastI am Grant Component Toast" 
+        variant="success"
+        index={index}
+        onClick={undoAction}
+      />
+    ));
+  })
+  .add('Multi toast success and action lallal', () => {
+    return [1, 2, 3].map((x, index) => (
+      <Toast
+        key={index}
+        label="hey there,I am Grant ToastI  Toast"
+        variant="success"
+        index={index}
+        onClick={undoAction}
+      />
+    ));
+  })
+  .add('with error', () => {
+    return <Toast label="hey  am Grant Component Toast" variant="error" />;
+  })
+  .add('with warning', () => {
+    return <Toast label="hey  am Grant Component Toast" variant="warning" />;
+  })
+  .add('with default', () => {
+    return <Toast label="hey  am Grant Component Toast" variant="original" />;
+  });
