5fa3cc48ffdaf53cc76b29dff8eef9d755756ce0
Ramin B <ramin@wework.com>
2019-10-02 10:13:57 -0400

    feat(core-react): change type to kind on Button

--- packages/core-react/src/components/Button/button.test.js
+++ packages/core-react/src/components/Button/button.test.js
@@ -6,13 +6,24 @@ import Button from '.';
 describe('Button', () => {
   test('it renders a primary button', () => {
     const wrapper = mount(<Button>hello world</Button>);
-    expect(wrapper.find('.ray-button--primary').length).toBe(1);
+    expect(wrapper.find('.ray-button--primary')).toHaveLength(1);
   });
 
   test('it supports a custom class', () => {
     const wrapper = mount(
       <Button className="some-custom-class">hello world</Button>
     );
-    expect(wrapper.find('.ray-button.some-custom-class').length).toBe(1);
+    expect(wrapper.find('.ray-button.some-custom-class')).toHaveLength(1);
+  });
+
+  test('it supports type & kind attributes', () => {
+    const wrapper = mount(
+      <Button kind="secondary" type="submit">
+        Submit Button
+      </Button>
+    );
+    const buttonWrapper = wrapper.find(Button);
+    expect(buttonWrapper.props().type).toEqual('submit');
+    expect(buttonWrapper.props().kind).toEqual('secondary');
   });
 });
