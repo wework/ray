74d93cf32b5f351cdc6fbf7771ad555a8ed2bc8e
James Etienne <james.etienne@wework.com>
2019-10-08 15:27:35 -0400

    removed folder

--- packages/product-react/src/components/Common/RTLWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This is needed to support rtl
-const RTLWrapper: React.FC<{ renderWrapper?: boolean }> = ({
-  children,
-  renderWrapper
-}) => (renderWrapper ? <div dir="rtl">{children}</div> : <>{children}</>);
-
-export default RTLWrapper;
