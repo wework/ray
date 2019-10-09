45ad21fd14dc47dd2cdd4c9af4ac27e40a44b852
James Etienne <james.etienne@wework.com>
2019-10-09 13:04:32 -0400

    add textArea to ray-product

---
+++ packages/product-react/stories/TextArea.story.tsx
@@ -0,0 +1,57 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { TextArea } from '../src/components/TextArea';
+
+storiesOf('TextArea', module)
+  .add('default', () => (
+    <TextArea
+      id="example"
+      value={undefined}
+      placeholder="hello"
+      label="im a label"
+    />
+  ))
+  .add('required', () => (
+    <TextArea
+      id="example"
+      value={undefined}
+      placeholder="hello"
+      required
+      label="im a label"
+    />
+  ))
+  .add('error', () => (
+    <TextArea
+      id="example"
+      value={undefined}
+      placeholder="hello"
+      error="something went wrong"
+    />
+  ))
+  .add('with value', () => (
+    <TextArea
+      id="example"
+      value="worlds"
+      placeholder="hello"
+      label="im a label"
+      onChange={() => 'Hello world!'}
+    />
+  ))
+  .add('disabled', () => (
+    <TextArea
+      id="example"
+      value="worlds"
+      placeholder="hello"
+      disabled
+      label="im a label"
+    />
+  ))
+  .add('compact', () => (
+    <TextArea
+      id="example"
+      value="worlds"
+      placeholder="hello"
+      compact
+      label="im a label"
+    />
+  ));
