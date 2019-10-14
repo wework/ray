28ff1dbd9a4bb69d6604a7fc3c83f24d0f25355b
Adam Raider <adamjraider@gmail.com>
2019-10-14 14:41:22 -0400

    Revert "converted checkbox from js to tsx in ray-product (#237)"
    This reverts commit 4bb1900ca9f907acdd823a0995b3d59ef291c21d.

--- packages/product-react/stories/Checkbox.story.tsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-
-import { Checkbox } from '../src/components/Checkbox';
-
-storiesOf('Checkbox', module).add('default', () => (
-  <Checkbox id="checkboxtest" label="henlo" />
-));
