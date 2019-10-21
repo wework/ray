03811785305b8732d93edb2a7997769a2cfd2dd0
James Etienne <james.etienne@wework.com>
2019-10-21 13:30:18 -0400

    added id requiered id props in test

--- packages/product-react/src/components/Checkbox/Checkbox.test.tsx
+++ packages/product-react/src/components/Checkbox/Checkbox.test.tsx
@@ -5,14 +5,14 @@ import Checkbox from './Checkbox';
 
 describe('Checkbox', () => {
   test('it renders a checkbox', () => {
-    const wrapper = Enzyme.mount(<Checkbox label="hello world" />);
+    const wrapper = Enzyme.mount(<Checkbox id="3" label="hello world" />);
     expect(wrapper.find('.ray-checkbox').length).toBe(1);
     expect(wrapper.find('input[type="checkbox"]').length).toBe(1);
   });
 
   test('it supports a custom class', () => {
     const wrapper = Enzyme.mount(
-      <Checkbox className="some-custom-class" id="checkboxtest" label="henlo" />
+      <Checkbox className="some-custom-class" id="4" label="henlo" />
     );
     expect(wrapper.find('.ray-checkbox.some-custom-class').length).toBe(1);
   });
