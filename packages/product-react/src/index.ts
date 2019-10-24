2d42c879ad07fefd72eda178fa0f6b2cddf78293
James D <45638740+jamesdamild@users.noreply.github.com>
2019-10-23 20:47:02 -0400

    Feat(ray-product):Added CARD in tsx format  (#243)
    * added CARD in tsx format in ray -product
    
    * removed commented code. exported components
    
    * add yarn.lock file
    
    * added test for card children
    
    * added variables  card scss
    
    * renamed class named css

--- packages/product-react/src/index.ts
+++ packages/product-react/src/index.ts
@@ -1,3 +1,7 @@
 export * from './components/Container';
 export * from './components/Grid';
 export * from './components/Tabs';
+export * from './components/Card';
+export * from './components/Select';
+export * from './components/Checkbox';
+export * from './components/TextArea';
