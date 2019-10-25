bb35a0fc1aef5258562785a54f0735c329b8e79e
James Etienne <james.etienne@wework.com>
2019-10-25 15:28:39 -0400

    added button from jsx -> tsx

--- packages/product-react/src/components/Button/Button.tsx
+++ packages/product-react/src/components/Button/Button.tsx
@@ -1,77 +1,65 @@
 import clsx from 'clsx';
-// import PropTypes from 'prop-types';
 import React, { createRef, forwardRef, ReactHTMLElement } from 'react';
 
 export type ButtonProps = {
-  Tag?: 'button' | 'a';
   children?: React.ReactNode;
-  type?: 'primary' | 'secondary' | 'tertiary';
+  buttonStyle?: 'primary' | 'secondary' | 'tertiary';
+  type?: 'button' | 'reset' | 'submit';
   compact?: boolean;
   danger?: boolean;
   disabled?: boolean;
   className?: string;
+  tabIndex?: number;
+  name?: string;
+  value?: string | string[] | number;
+  onClick?(event: React.MouseEvent<HTMLButtonElement>): void;
 };
 
-// export type Ref = HTMLElement;
-// type LegacyRef<T> = string | Ref<T>;
+export const Button = React.forwardRef(
+  (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => {
+    const {
+      name,
+      value,
+      children,
+      type,
+      compact,
+      danger,
+      disabled,
+      className,
+      buttonStyle,
+      ...rest
+    } = props;
 
-type Ref =
-  | JSX.IntrinsicElements['a']['ref']
-  | JSX.IntrinsicElements['button']['ref'];
-// Which is a union of functions!
-declare var ref: Ref;
-// (Let’s ignore string refs)
-// if (typeof ref === 'function') {
-//   // So it wants `HTMLButtonElement & HTMLAnchorElement`
-//   ref(new React.RefObject<HTMLInputElement>);
-//   ref(new React.RefObject<HTMLInputElement>);
-//}
-
-
-
-
-
-export const Button = React.forwardRef((props: ButtonProps,) => {
-  const {
-    Tag = 'button',
-    children,
-    type,
-    compact,
-    danger,
-    disabled,
-    className,
-    /// <reference path="" />
-
-    ...rest
-  } = props;
-
-  //  const innerRef = ref;
-
-  return (
-    <Tag
-      {...rest}
-      className={clsx(
-        'ray-button',
-        `ray-button--${type}`,
-        {
-          'ray-button--compact': compact,
-          'ray-button--danger': danger
-        },
-        className
-      )}
-      disabled={disabled}
-      ref={ref}
-    >
-      {children}
-    </Tag>
-  );
-});
+    return (
+      <button
+        name={name}
+        value={value}
+        type={type}
+        {...rest}
+        className={clsx(
+          'ray-button',
+          `ray-button--${buttonStyle}`,
+          {
+            'ray-button--compact': compact,
+            'ray-button--danger': danger
+          },
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
 
 Button.defaultProps = {
-  Tag: 'button',
-  type: 'primary',
+  buttonStyle: 'primary',
   compact: false,
-  danger: false
+  danger: false,
+  disabled: false
 };
 
 Button.displayName = 'Button';
