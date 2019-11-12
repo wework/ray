fa23dd2aa75da0ded06f430ee7a5c68f5d24b965
James D <45638740+jamesdamild@users.noreply.github.com>
2019-11-12 10:18:16 -0500

    Feat: Added avatar component  to product-react (#251)

---
+++ packages/product-react/src/components/Avatar/Avatar.test.tsx
@@ -0,0 +1,164 @@
+import Enzyme, { configure, mount, ReactWrapper } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { Avatar } from './Avatar';
+
+configure({ adapter: new Adapter() });
+
+describe('Avatar', () => {
+  let component: ReactWrapper<any>;
+  const fauxCallback = jest.fn();
+
+  beforeEach(() => {
+    component = mount(<Avatar />);
+  });
+
+  test('should match snapshot and styles', () => {
+    expect(mount(<Avatar />)).toMatchSnapshot();
+  });
+
+  describe('Default Properties', () => {
+    it('has default properties "add" that can be set', () => {
+      expect(component.props().add).toEqual(false);
+    });
+    it('has default properties "edit" that can be set', () => {
+      expect(component.props().edit).toEqual(false);
+    });
+    it('has default properties "size" that can be set', () => {
+      expect(component.props().size).toEqual('medium');
+    });
+    it('has no defaulted image', () => {
+      expect(component.props().image).toEqual(undefined);
+      expect(
+        component
+          .find('img')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+    });
+    it('has no default "add/edit" icon', () => {
+      expect(component.props().add).toEqual(false);
+      expect(component.props().edit).toEqual(false);
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+    });
+    it('has initial of name when specified', () => {
+      component.setProps({
+        name: 'Patrick Stewart',
+      });
+      expect(component.find('img').exists()).toEqual(false);
+      expect(
+        component
+          .find('span')
+          .at(0)
+          .contains('P'),
+      ).toEqual(true);
+    });
+    it('defaults to display image avatar if both image and name is specified', () => {
+      component.setProps({
+        image:
+          'https://pbs.twimg.com/profile_images/624249118114881536/qxn_I_oR_400x400.jpg',
+        name: 'Patrick Stewart',
+      });
+      expect(
+        component
+          .find('span')
+          .at(0)
+          .contains('P'),
+      ).toEqual(false);
+      expect(component.find('img').exists()).toEqual(true);
+    });
+  });
+
+  describe('Classes', () => {
+    it('contains DEFAULT class', () => {
+      expect(
+        component
+          .find('.lucia-avatar')
+          .at(0)
+          .exists(),
+      ).toEqual(true);
+    });
+    it('contains nested "add" icon (ONLY for size="large")', () => {
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+      component.setProps({ add: true });
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+      component.setProps({ add: true, size: 'small' });
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+      component.setProps({ add: true, size: 'large' });
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(true);
+    });
+    it('contains nested image', () => {
+      expect(
+        component
+          .find('.lucia-avatar--action')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+      component.setProps({
+        image:
+          'https://pbs.twimg.com/profile_images/624249118114881536/qxn_I_oR_400x400.jpg',
+      });
+      expect(
+        component
+          .find('img')
+          .at(0)
+          .exists(),
+      ).toEqual(true);
+    });
+    it('contains correct class for "size" variant ', () => {
+      expect(
+        component
+          .find('.lucia-avatar--small')
+          .at(0)
+          .exists(),
+      ).toEqual(false);
+      component.setProps({ size: 'small' });
+      expect(component.find('.lucia-avatar--small').exists()).toEqual(true);
+      component.setProps({ size: 'medium' });
+      expect(component.find('.lucia-avatar--medium').exists()).toEqual(true);
+      component.setProps({ size: 'large' });
+      expect(component.find('.lucia-avatar--large').exists()).toEqual(true);
+      component.setProps({ size: 'gobblygook' });
+      expect(component.find('.lucia-avatar--small').exists()).toEqual(false);
+      expect(component.find('.lucia-avatar--medium').exists()).toEqual(false);
+      expect(component.find('.lucia-avatar--large').exists()).toEqual(false);
+    });
+  });
+
+  describe('Functions', () => {
+    it('clicks (ONLY for size="large" w/ "add" icon and passed action)', () => {
+      component.setProps({
+        onAddImageClick: fauxCallback,
+        add: true,
+        size: 'large',
+      });
+      component.find('.lucia-avatar--action').simulate('click');
+      expect(fauxCallback.mock.calls.length).toEqual(1);
+    });
+  });
+});
---
+++ packages/product-react/stories/Avatar.story.tsx
@@ -0,0 +1,95 @@
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { Avatar } from '../src/components/Avatar';
+
+const fauxAvatarImage =
+  'https://pbs.twimg.com/profile_images/624249118114881536/qxn_I_oR_400x400.jpg';
+
+storiesOf('Core Comps | Avatar', module)
+  .add('no avatar - small', () => {
+    return <Avatar size="small" name='small' />;
+  })
+  .add('no avatar - medium (default size)', () => {
+    return <Avatar />;
+  })
+  .add('no avatar - large', () => {
+    return <Avatar size="large" />;
+  })
+  .add('avatar - small', () => {
+    return <Avatar image={fauxAvatarImage} size="small" />;
+  })
+  .add('avatar - medium (default size)', () => {
+    return <Avatar image={fauxAvatarImage} />;
+  })
+  .add('avatar - large', () => {
+    return <Avatar image={fauxAvatarImage} size="large" />;
+  })
+  .add('no avatar - large (add)', () => {
+    return <Avatar size="large" add />;
+  })
+  .add('no avatar - large (edit)', () => {
+    return <Avatar size="large" edit />;
+  })
+  .add('avatar - large (add)', () => {
+    return <Avatar size="large" image={fauxAvatarImage} add />;
+  })
+  .add('avatar - large (edit)', () => {
+    return <Avatar size="large" image={fauxAvatarImage} edit />;
+  })
+  .add('no avatar - small w/ initial', () => {
+    return <Avatar size="small" name="Patrick Stewart" />;
+  })
+  .add('no avatar - medium (default size) w/ initial', () => {
+    return <Avatar name="Patrick Stewart" />;
+  })
+  .add('no avatar - large w/ initial', () => {
+    return <Avatar size="large" name="Patrick Stewart" />;
+  })
+  .add('multiple avatars', () => {
+    return (
+      <>
+        <Avatar size="small" />
+        <Avatar />
+        <Avatar size="large" />
+
+        <Avatar size="small" name="Patrick Stewart" />
+        <Avatar name="Patrick Stewart" />
+        <Avatar size="large" name="Patrick Stewart" />
+
+        <Avatar size="small" add />
+        <Avatar add />
+        <Avatar size="large" add />
+
+        <Avatar size="small" edit />
+        <Avatar edit />
+        <Avatar size="large" edit />
+
+        <Avatar size="small" name="Patrick Stewart" add />
+        <Avatar name="Patrick Stewart" add />
+        <Avatar size="large" name="Patrick Stewart" add />
+
+        <Avatar size="small" image={fauxAvatarImage} add />
+        <Avatar image={fauxAvatarImage} add />
+        <Avatar size="large" image={fauxAvatarImage} add />
+
+        <Avatar size="small" image={fauxAvatarImage} edit />
+        <Avatar image={fauxAvatarImage} edit />
+        <Avatar size="large" image={fauxAvatarImage} edit />
+
+        <Avatar
+          size="small"
+          image={fauxAvatarImage}
+          add
+          name="Patrick Stewart"
+        />
+        <Avatar image={fauxAvatarImage} add name="Patrick Stewart" />
+        <Avatar
+          size="large"
+          image={fauxAvatarImage}
+          add
+          name="Patrick Stewart"
+        />
+      </>
+    );
+  });
