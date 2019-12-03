4b238ba2fdbc9b6f073f037ea6b1797e9471f00c
Celeste Glavin <celeste.glavin@wework.com>
2019-12-02 20:50:31 -0500

    fix(docs-deploy): update how core-js is imported (#256)
    * test
    
    * update the path to polyfills
    
    * update the path to polyfills everywhere
    
    * remove test comment

--- packages/docs-app/src/polyfills/index.js
+++ packages/docs-app/src/polyfills/index.js
@@ -1,15 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import 'core-js/modules/es.array.fill';
-import 'core-js/modules/es.array.from';
-import 'core-js/modules/es7.array.includes';
-import 'core-js/modules/es.math.sign';
-import 'core-js/modules/es.object.assign';
-import 'core-js/modules/es7.object.values';
-import 'core-js/modules/es.promise';
-import 'core-js/modules/es.string.includes';
-import 'core-js/modules/es.string.trim';
-import 'core-js/modules/es.symbol';
-/* eslint-enable import/no-extraneous-dependencies */
+import 'core-js';
 
 import './custom-event';
 import './element-closest';
